b8a3f1c34ed09f9cc13669c3a452d66c43603c98
Marton Tasnadi <marton.tasnadi@gmail.com>
2021-01-03 19:12:08 +0100

    Fix indentation.

--- src/CountySeats.php
+++ src/CountySeats.php
@@ -6,24 +6,24 @@ class CountySeats
 {
     const COUNTY_CITY_MAP = [
         'Bács-Kiskun' => 'Kecskemét',
-		'Baranya' => 'Pécs',
-		'Békés' => 'Békéscsaba',
-		'Borsod-Abaúj-Zemplén' => 'Miskolc',
-		'Csongrád-Csanád' => 'Szeged',
-		'Fejér' => 'Székesfehérvár',
-		'Győr-Moson-Sopron' => 'Győr',
-		'Hajdú-Bihar' => 'Debrecen',
-		'Heves' => 'Eger',
-		'Jász-Nagykun-Szolnok' => 'Szolnok',
-		'Komárom-Esztergom' => 'Tatabánya',
-		'Nógrád' => 'Salgótarján',
-		'Pest' => 'Budapest',
-		'Somogy' => 'Kaposvár',
-		'Szabolcs-Szatmár-Bereg' => 'Nyíregyháza',
-		'Tolna' => 'Szekszárd',
-		'Vas' => 'Szombathely',
-		'Veszprém' => 'Veszprém',
-		'Zala' => 'Zalaegerszeg'
+        'Baranya' => 'Pécs',
+        'Békés' => 'Békéscsaba',
+        'Borsod-Abaúj-Zemplén' => 'Miskolc',
+        'Csongrád-Csanád' => 'Szeged',
+        'Fejér' => 'Székesfehérvár',
+        'Győr-Moson-Sopron' => 'Győr',
+        'Hajdú-Bihar' => 'Debrecen',
+        'Heves' => 'Eger',
+        'Jász-Nagykun-Szolnok' => 'Szolnok',
+        'Komárom-Esztergom' => 'Tatabánya',
+        'Nógrád' => 'Salgótarján',
+        'Pest' => 'Budapest',
+        'Somogy' => 'Kaposvár',
+        'Szabolcs-Szatmár-Bereg' => 'Nyíregyháza',
+        'Tolna' => 'Szekszárd',
+        'Vas' => 'Szombathely',
+        'Veszprém' => 'Veszprém',
+        'Zala' => 'Zalaegerszeg'
     ];
 
     public static function all(): array
